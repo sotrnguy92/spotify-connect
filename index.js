5028bbfc15b8ac1828567a30fa96876318024092
sotrnguy92 <sonnguyen092@gmail.com>
2022-01-07 00:47:51 -0800

    finishing authorization routes for logging in to spotify account and routes for refreshing OAuth tokens

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import axios from "axios";
-
+const axios = require('axios')
 require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring');
 const port = 8888;
-const axios = require('axios')
 
 const app = express();
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -52,9 +50,69 @@ app.get('/login', (req, res) => {
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`)
 })
 
-app.get('/callback', (req,res) => {
-    res.send('callback')
-})
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+        },
+    })
+        .then(response => {
+            if (response.status === 200) {
+
+                const { access_token, token_type } = response.data;
+
+                const { refresh_token } = response.data;
+
+                axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
+                    .then(response => {
+                        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+                    })
+                    .catch(error => {
+                        res.send(error);
+                    });
+
+            } else {
+                res.send(response);
+            }
+        })
+        .catch(error => {
+            res.send(error);
+        });
+});
+
+
+app.get('/refresh_token', (req, res) => {
+    const { refresh_token } = req.query;
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+        },
+    })
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            res.send(error);
+        });
+});
 
 app.listen(port, () => {
     console.log(`server listening on port ` + port + `at http://localhost:${port}`)
