6b7351ba26c6807f91b117873cb9fbef0bf7e1e6
sotrnguy92 <sonnguyen092@gmail.com>
2022-01-11 20:17:45 -0800

    addding description comments and adding the hasTokenExpired utility function

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -6,15 +6,28 @@ const LOCALSTORAGE_KEYS = {
     timestamp: 'spotify_token_timestamp',
 }
 
+// creating object to hold token values and timestamps. If nothing in local storage then values should be "undefined"
 const LOCALSTORAGE_VALUES = {
-    accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.access_token),
-    refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refresh_token),
-    expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expire_time),
-    timeStamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timeStamp),
+    accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+    refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+    expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+    timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
 }
 
+
+const hasTokenExpired = () => {
+    const {accessToken, timestamp, expireTime} = LOCALSTORAGE_VALUES;
+    if(!accessToken || !timestamp){
+        return false;
+    }
+
+    const timeElapsed = Date.now() - Number(timestamp);
+    return (timeElapsed/1000) > Number(expireTime);
+}
+
+
 const getAccessToken = () => {
-    //first get the
+    //first get the query values from the url and store them in queryParams. If there is no query then the values should be undefined
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString)
     const queryParams = {
@@ -25,22 +38,24 @@ const getAccessToken = () => {
 
     const hasError = urlParams.get('error');
 
+    //if there is an error or the access token has expired then refresh the access token.
     if (hasError || hasTokenExpired() || LOCALSTORAGE_VALUES.accessToken === 'undefined') {
         refreshToken();
         return LOCALSTORAGE_VALUES.accessToken;
     }
 
+    //if there is a valid access token in local storage then return that token to use
     if(LOCALSTORAGE_VALUES.accessToken && LOCALSTORAGE_VALUES.accessToken !== 'undefined'){
         return LOCALSTORAGE_VALUES.accessToken;
     }
 
+    // Otherwise the user is logging in for the first time and there are tokens in the query params.
     if(queryParams[LOCALSTORAGE_KEYS.accessToken]){
-
         for (const property in queryParams){
             window.localStorage.setItem(property, queryParams[property]);
         }
 
-        window.localStorage.setItem(LOCALSTORAGE_KEYS.timeStamp, Date.now());
+        window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp,  Date.now());
 
         return queryParams[LOCALSTORAGE_KEYS.accessToken];
     }
