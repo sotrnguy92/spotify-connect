335a01c5959310ca425d98e73102703db16e12d0
sotrnguy92 <sonnguyen092@gmail.com>
2022-01-11 19:44:08 -0800

    updating spotify.js logic to handle refreshing an access token, storing the access token, and logging in for the first time

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,25 +1,52 @@
+// create a legend for local storage keys
 const LOCALSTORAGE_KEYS = {
-    access_token: 'spotify_access_token',
-    refresh_token: 'spotify_refresh_token',
-    expire_time: 'spotify_token_expire_time',
+    accessToken: 'spotify_access_token',
+    refreshToken: 'spotify_refresh_token',
+    expireTime: 'spotify_token_expire_time',
     timestamp: 'spotify_token_timestamp',
 }
 
 const LOCALSTORAGE_VALUES = {
-    access_token: window.localStorage.getItem(LOCALSTORAGE_KEYS.access_token),
-    refresh_token: window.localStorage.getItem(LOCALSTORAGE_KEYS.refresh_token),
-    expire_time: window.localStorage.getItem(LOCALSTORAGE_KEYS.expire_time),
+    accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.access_token),
+    refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refresh_token),
+    expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expire_time),
     timeStamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timeStamp),
 }
 
 const getAccessToken = () => {
+    //first get the
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString)
+    const queryParams = {
+        [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('access_token'),
+        [LOCALSTORAGE_KEYS.refreshToken]: urlParams.get('refresh_token'),
+        [LOCALSTORAGE_KEYS.expireTime]: urlParams.get('expires_in'),
+    };
 
-    const access_token = urlParams.get('access_token')
-    const refresh_token = urlParams.get('refresh_token')
+    const hasError = urlParams.get('error');
 
-    return access_token;
+    if (hasError || hasTokenExpired() || LOCALSTORAGE_VALUES.accessToken === 'undefined') {
+        refreshToken();
+        return LOCALSTORAGE_VALUES.accessToken;
+    }
+
+    if(LOCALSTORAGE_VALUES.accessToken && LOCALSTORAGE_VALUES.accessToken !== 'undefined'){
+        return LOCALSTORAGE_VALUES.accessToken;
+    }
+
+    if(queryParams[LOCALSTORAGE_KEYS.accessToken]){
+
+        for (const property in queryParams){
+            window.localStorage.setItem(property, queryParams[property]);
+        }
+
+        window.localStorage.setItem(LOCALSTORAGE_KEYS.timeStamp, Date.now());
+
+        return queryParams[LOCALSTORAGE_KEYS.accessToken];
+    }
+
+    return false;
+
 }
 
 export const access_token = getAccessToken();
